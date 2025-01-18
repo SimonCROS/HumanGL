e5e45402fa2c63f721d88d2cadde7f3dd1b83f2b
Simon Cros <simon.dc.cros@gmail.com>
2025-01-18 23:22:01 +0100

    Format Shader.cpp and Shader.h, optimize attribute cache

--- src/OpenGL/Shader.h
+++ src/OpenGL/Shader.h
@@ -14,51 +14,52 @@
 #include <glm/gtc/type_ptr.hpp>
 #include "glad/gl.h"
 
-struct ShaderProgramSource {
+struct ShaderProgramSource
+{
     std::string vertex;
     std::string fragment;
 };
 
-class Shader {
-
+class Shader
+{
 private:
     std::string m_filepath;
     unsigned int m_rendererID;
     std::unordered_map<std::string, int> m_uniformLocationCache;
 
-
 public:
-    explicit Shader(const std::string &filepath);
+    explicit Shader(const std::string& filepath);
 
-    ~Shader() {
+    ~Shader()
+    {
         glDeleteProgram(m_rendererID);
     };
 
-    void bind() const {
+    void bind() const
+    {
         glUseProgram(m_rendererID);
     };
 
-    void unbind() const {
+    void unbind() const
+    {
         glUseProgram(0);
     };
 
     // Set uniforms
-    void setUniform1i(const std::string &name, int value);
-    void setUniform1f(const std::string &name, float value);
-    void setUniform4f(const std::string &name, float v0, float v1, float v2, float v3);
-    void setUniformMat4f(const std::string &name, const glm::mat4 &matrix);
+    void setUniform1i(const std::string& name, int value);
+    void setUniform1f(const std::string& name, float value);
+    void setUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+    void setUniformMat4f(const std::string& name, const glm::mat4& matrix);
 
 private:
-    int _getUniformLocation(const std::string &name);
-    ShaderProgramSource _parseShader(const std::string &filepath);
-    unsigned int _compileShader(unsigned int type, const std::string &source);
-    unsigned int _createShader(const std::string &vertexShader, const std::string &fragmentShader);
-
+    int _getUniformLocation(const std::string& name);
+    ShaderProgramSource _parseShader(const std::string& filepath);
+    unsigned int _compileShader(unsigned int type, const std::string& source);
+    unsigned int _createShader(const std::string& vertexShader, const std::string& fragmentShader);
 };
 
 /* ----- Displaying Tools ----- */
-std::ostream &operator<<(std::ostream &o, const ShaderProgramSource &righty);
-
+std::ostream& operator<<(std::ostream& o, const ShaderProgramSource& righty);
 
 
 #endif //SHADER_H
